d94bc38f3814507c4ae13832ef7b15e2680c831d
Jon Mumm <jonathanrmumm@gmail.com>
2012-03-27 16:33:44 -0700

    Added install script

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 rm ~/.vimrc
+rm ~/.vimrc.before
+rm ~/.vimrc.after
+
 ln -s ~/.vim/janus/vim/vimrc ~/.vimrc
+ln -s ~/.vim/custom/.vimrc.before ~/.vimrc.before
+ln -s ~/.vim/custom/.vimrc.after ~/.vimrc.after
+
+rm ~/.janus
+ln -s ~/.vim/custom/janus ~/.janus
