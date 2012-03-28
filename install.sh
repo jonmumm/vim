570bec0954aa8b8c3881400ab80df231cd97fb12
Jon Mumm <jonathanrmumm@gmail.com>
2012-03-27 21:42:54 -0700

    Added submodules and moved some stuff

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ rm ~/.vimrc.before
 rm ~/.vimrc.after
 
 ln -s ~/.vim/janus/vim/vimrc ~/.vimrc
-ln -s ~/.vim/custom/.vimrc.before ~/.vimrc.before
-ln -s ~/.vim/custom/.vimrc.after ~/.vimrc.after
+ln -s ~/.vim/.vimrc.before ~/.vimrc.before
+ln -s ~/.vim/.vimrc.after ~/.vimrc.after
 
 rm ~/.janus
-ln -s ~/.vim/custom/janus ~/.janus
+ln -s ~/.vim/plugins ~/.janus
 
 mkdir ~/.vim/_backup
 mkdir ~/.vim/_temp
